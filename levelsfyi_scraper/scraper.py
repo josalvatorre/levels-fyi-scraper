c8d20fdae5d709a065a2e976f47ec79624b0911c
Jose Alvarado <jos.alvatorre@gmail.com>
2022-05-24 16:21:29 -0700

    dump company directory

--- levelsfyi_scraper/scraper.py
+++ levelsfyi_scraper/scraper.py
@@ -7,8 +7,8 @@ from .ResponseCache import ResponseCache
 cache_dir = pathlib.Path(pathlib.Path.cwd(), "cache_dir")
 cache_dir.mkdir(exist_ok=True)
 
-for _ in range(2):
-    response = ResponseCache(cache_dir).get("https://www.levels.fyi/company/")
-    data = bs4.BeautifulSoup(response.text, "html.parser")
-    print(len(data.body))
-    pass
+company_directory = bs4.BeautifulSoup(
+    ResponseCache(cache_dir).get("https://www.levels.fyi/company/").text,
+    "html.parser",
+)
+print(company_directory.prettify())
