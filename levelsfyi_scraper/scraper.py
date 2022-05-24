ac955040b44e60f79c21c9f17bb6d1a00f7b3fb5
Jose Alvarado <jos.alvatorre@gmail.com>
2022-05-24 16:25:59 -0700

    cached_requester

--- levelsfyi_scraper/scraper.py
+++ levelsfyi_scraper/scraper.py
@@ -6,9 +6,10 @@ from .ResponseCache import ResponseCache
 
 cache_dir = pathlib.Path(pathlib.Path.cwd(), "cache_dir")
 cache_dir.mkdir(exist_ok=True)
+cached_requester = ResponseCache(cache_dir)
 
 company_directory = bs4.BeautifulSoup(
-    ResponseCache(cache_dir).get("https://www.levels.fyi/company/").text,
+    cached_requester.get("https://www.levels.fyi/company/").text,
     "html.parser",
 )
 print(company_directory.prettify())
